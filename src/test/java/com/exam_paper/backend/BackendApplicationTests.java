ee3ace7b1e2dfd8175a255e24c02d04fde21227b
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-04 11:39:40 +0530

    initializing

---
+++ src/test/java/com/exam_paper/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam_paper.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
