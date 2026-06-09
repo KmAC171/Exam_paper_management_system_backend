cd0cacdfd7add4349bcd3835f6871713c21a134d
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-09 16:46:22 +0530

    User entity to use @Data annotation and add Role enum for user roles

--- src/main/java/com/exam_paper/backend/entity/User.java
+++ src/main/java/com/exam_paper/backend/entity/User.java
@@ -5,15 +5,28 @@ import lombok.*;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
+
+    public enum Role {
+        ROLE_ADMIN,
+        ROLE_MODERATOR,
+        ROLE_USER,
+        ROLE_GUEST
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @Column(nullable = false)
     private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
 }
