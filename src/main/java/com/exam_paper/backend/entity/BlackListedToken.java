ffb9195b5ca6a98c3bebb67b77fc1820a5076df0
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-15 22:54:10 +0530

    Add BlackListedToken entity with fields for token and expiration

--- src/main/java/com/exam_paper/backend/entity/BlackListedToken.java
+++ src/main/java/com/exam_paper/backend/entity/BlackListedToken.java
@@ -1,4 +1,25 @@
 package com.exam_paper.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "blacklisted_tokens")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BlackListedToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 512)
+    private String token;
+
+    @Column(nullable = false)
+    private Data expiresAt;
 }
