913abba9cacd8943981aebf9257266fe446c54ec
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-15 23:52:52 +0530

    Implement blacklistToken method in TokenBlacklistService for token management

--- src/main/java/com/exam_paper/backend/service/TokenBlacklistService.java
+++ src/main/java/com/exam_paper/backend/service/TokenBlacklistService.java
@@ -1,4 +1,18 @@
 package com.exam_paper.backend.service;
 
+import com.exam_paper.backend.repository.BlackListedTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
 public class TokenBlacklistService {
+    private final BlackListedTokenRepository blackListedTokenRepository;
+
+    public void blacklistToken (String token, Date expiresAt){
+        
+    }
+
 }
