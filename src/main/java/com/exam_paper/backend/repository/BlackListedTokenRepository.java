e63f1c50e70afcfa17e371735f673ff07c0c6f3f
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-15 23:12:39 +0530

    Refactor BlackListedTokenRepository to extend JpaRepository and add methods for token management

--- src/main/java/com/exam_paper/backend/repository/BlackListedTokenRepository.java
+++ src/main/java/com/exam_paper/backend/repository/BlackListedTokenRepository.java
@@ -1,4 +1,19 @@
 package com.exam_paper.backend.repository;
 
-public interface BlackListedTokenRepository {
+import com.exam_paper.backend.entity.BlackListedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.xml.crypto.Data;
+
+public interface BlackListedTokenRepository extends JpaRepository<BlackListedToken, Long> {
+    boolean existsByToken(String token);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM BlacklistedToken b WHERE b.expiresAt < :now")
+    void deleteExpiredTokens(Data now);
+
 }
