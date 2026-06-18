a7928283312001aa33c0ce4729c65e551d95f1fa
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-18 23:46:28 +0530

    service addded

--- src/main/java/com/exam_paper/backend/service/TokenBlacklistService.java
+++ src/main/java/com/exam_paper/backend/service/TokenBlacklistService.java
@@ -1,7 +1,9 @@
 package com.exam_paper.backend.service;
 
+import com.exam_paper.backend.entity.BlackListedToken;
 import com.exam_paper.backend.repository.BlackListedTokenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -12,7 +14,21 @@ public class TokenBlacklistService {
     private final BlackListedTokenRepository blackListedTokenRepository;
 
     public void blacklistToken (String token, Date expiresAt){
-        
+        BlackListedToken blacklisted = BlackListedToken.builder()
+                .token(token)
+                .expiresAt(expiresAt)
+                .build();
+        blackListedTokenRepository.save(blacklisted);
+    }
+
+    public boolean isBlacklisted(String token){
+        return blackListedTokenRepository.existsByToken(token);
+    }
+
+    @Scheduled(fixedRate = 360000)
+    public void cleanExpiredTokens() {
+        blackListedTokenRepository.deleteExpiredTokens(new Date());
+        System.out.println(">>> Cleaned up expired blacklisted tokens");
     }
 
 }
