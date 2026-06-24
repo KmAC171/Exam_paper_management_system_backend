80f14556fe63128c39d330a6aac1d4fe7e5174dc
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-24 21:18:08 +0530

    add properties to DashboardSummaryDTO class

--- src/main/java/com/exam_paper/backend/dto/DashboardSummaryDTO.java
+++ src/main/java/com/exam_paper/backend/dto/DashboardSummaryDTO.java
@@ -1,4 +1,16 @@
 package com.exam_paper.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DashboardSummaryDTO {
+    private long totalPackets;
+    private long pending;
+    private long approved;
+    private long delayed;
+    private long printingQueue;
 }
