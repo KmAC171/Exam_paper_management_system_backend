4b77e075a389a35efa11ea0b691a30a939b47f9f
KmAC171 <kmcmadushan2002@gmail.com>
2026-06-26 10:45:35 +0530

    add countDelayed method to ExamPacketRepository and update ARDashboardService to include summary calculations

--- src/main/java/com/exam_paper/backend/repository/ExamPacketRepository.java
+++ src/main/java/com/exam_paper/backend/repository/ExamPacketRepository.java
@@ -2,10 +2,17 @@ package com.exam_paper.backend.repository;
 
 import com.exam_paper.backend.entity.ExamPacket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ExamPacketRepository extends JpaRepository<ExamPacket, Long> {
 
     List<ExamPacket> findByLecturerUserId(Long lecturerId);
+    long countByStatus_StatusName(String statusName);
+
+    @Query("SELECT COUNT(p) FROM ExamPacket p " + "WHERE p.status.statusName = 'PENDING'AND p.deadline < :today ")
+    long countDelayed(@Param("today") LocalDate today);
 }
