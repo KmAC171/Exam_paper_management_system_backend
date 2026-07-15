e7cf43e5b061264f9aac2d6873b4574084a625bf
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-07-15 11:40:56 +0530

    StaffPerformanceResponseDTO

--- src/main/java/com/exam_paper/backend/dto/StaffPerformanceResponseDTO.java
+++ src/main/java/com/exam_paper/backend/dto/StaffPerformanceResponseDTO.java
@@ -1,4 +1,25 @@
 package com.exam_paper.backend.dto;
 
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StaffPerformanceResponseDTO {
+
+
+    private Long userId;
+
+    private String staffName;
+
+    private String role;
+
+    private Long completedPackets;
+
+    private Long pendingPackets;
+
+    private Long overduePackets;
+
 }
