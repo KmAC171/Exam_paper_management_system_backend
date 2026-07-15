51d8491f90aabfeef5409d703dec6984f46563f1
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-07-15 11:55:18 +0530

    Added Commentrepository

---
+++ src/main/java/com/exam_paper/backend/repository/CommentRepository.java
@@ -0,0 +1,24 @@
+package com.exam_paper.backend.repository;
+
+
+import com.exam_paper.backend.entity.Comment;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+
+
+public interface CommentRepository
+        extends JpaRepository<Comment, Long> {
+
+
+
+    List<Comment> findByPacketPacketIdOrderByTimestampDesc(
+            Long packetId
+    );
+
+
+}
