d11c6223cee8a05855b9a61c526dea8ed3395d5a
KmAC171 <kmcmadushan2002@gmail.com>
2026-07-27 23:50:54 +0530

    added PacketController files done

--- src/main/java/com/exam_paper/backend/controller/PacketController.java
+++ src/main/java/com/exam_paper/backend/controller/PacketController.java
@@ -1,4 +1,25 @@
 package com.exam_paper.backend.controller;
 
+import com.exam_paper.backend.dto.PacketDTO;
+import com.exam_paper.backend.service.PacketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/packets")
+@RequiredArgsConstructor
 public class PacketController {
+
+    private final PacketService packetService;
+
+    @GetMapping
+    public List<PacketDTO> getPackets(Authentication authentication) {
+        String username = authentication.getName();
+        String role = authentication.getAuthorities()
+                .iterator().next().getAuthority();
+        return packetService.getPackets(username, role);
+    }
 }
