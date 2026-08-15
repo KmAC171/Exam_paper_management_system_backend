da65352014d670bb900eae28a3293ab8ff1edd6b
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-08-15 19:50:25 +0530

    Lecture comment controller Added

--- src/main/java/com/exam_paper/backend/controller/lecturer/LecturerCommentController.java
+++ src/main/java/com/exam_paper/backend/controller/lecturer/LecturerCommentController.java
@@ -1,4 +1,40 @@
 package com.exam_paper.backend.controller.lecturer;
 
+import com.exam_paper.backend.dto.lecturer.*;
+import com.exam_paper.backend.service.lecturer.LecturerCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lecturer")
+@RequiredArgsConstructor
+@CrossOrigin
 public class LecturerCommentController {
+
+    private final LecturerCommentService lecturerCommentService;
+
+    @PostMapping("/comments")
+    public ResponseEntity<CommentResponseDTO> addComment(
+            @RequestBody CommentRequestDTO request
+    ) {
+        return ResponseEntity.ok(
+                lecturerCommentService.addComment(
+                        request.getPacketId(),
+                        request.getUserId(),
+                        request.getCommentText()
+                )
+        );
+    }
+
+    @GetMapping("/comments/{packetId}")
+    public ResponseEntity<List<CommentResponseDTO>> getPacketComments(
+            @PathVariable String packetId
+    ) {
+        return ResponseEntity.ok(
+                lecturerCommentService.getPacketComments(packetId)
+        );
+    }
 }
