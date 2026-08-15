76c4291ba8de40b3025c4b705e47a0233c4a5667
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-08-15 19:51:11 +0530

    Lecture Notification controller Added

--- src/main/java/com/exam_paper/backend/controller/lecturer/LecturerNotificationController.java
+++ src/main/java/com/exam_paper/backend/controller/lecturer/LecturerNotificationController.java
@@ -1,4 +1,27 @@
 package com.exam_paper.backend.controller.lecturer;
 
+import com.exam_paper.backend.dto.lecturer.NotificationResponseDTO;
+import com.exam_paper.backend.service.lecturer.LecturerNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lecturer")
+@RequiredArgsConstructor
+@CrossOrigin
 public class LecturerNotificationController {
+
+    private final LecturerNotificationService lecturerNotificationService;
+
+    @GetMapping("/{userId}/notifications")
+    public ResponseEntity<List<NotificationResponseDTO>> getNotifications(
+            @PathVariable String userId
+    ) {
+        return ResponseEntity.ok(
+                lecturerNotificationService.getNotifications(userId)
+        );
+    }
 }
