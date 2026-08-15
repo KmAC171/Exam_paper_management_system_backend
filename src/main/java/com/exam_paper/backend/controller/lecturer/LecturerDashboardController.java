92b29cff5772f43939ed0b3da1041cfbd84d21f3
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-08-15 19:49:44 +0530

    Lecture Dashboard controller Added

--- src/main/java/com/exam_paper/backend/controller/lecturer/LecturerDashboardController.java
+++ src/main/java/com/exam_paper/backend/controller/lecturer/LecturerDashboardController.java
@@ -1,4 +1,25 @@
 package com.exam_paper.backend.controller.lecturer;
 
+import com.exam_paper.backend.dto.lecturer.LecturerDashboardResponseDTO;
+import com.exam_paper.backend.service.lecturer.LecturerDashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/lecturer")
+@RequiredArgsConstructor
+@CrossOrigin
 public class LecturerDashboardController {
+
+    private final LecturerDashboardService lecturerDashboardService;
+
+    @GetMapping("/{lecturerId}/dashboard")
+    public ResponseEntity<LecturerDashboardResponseDTO> getDashboard(
+            @PathVariable String lecturerId
+    ) {
+        return ResponseEntity.ok(
+                lecturerDashboardService.getDashboard(lecturerId)
+        );
+    }
 }
