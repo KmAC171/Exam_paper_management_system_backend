43ba35f98d6c75640e76aca509dfbb6bb7f18fdb
KmAC171 <kmcmadushan2002@gmail.com>
2026-08-16 22:54:36 +0530

    updated user dto with isactive and other new ly added characteristics

--- src/main/java/com/exam_paper/backend/dto/UserDTO.java
+++ src/main/java/com/exam_paper/backend/dto/UserDTO.java
@@ -1,11 +1,22 @@
 package com.exam_paper.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+    private Long userId;
+    private String fullName;
     private String username;
-    private String password;
-    private String fullname;
+    private String email;
     private String role;
+    private String roleLabel;
+    private String department;
+    private boolean isActive;
+    private String lastLogin;
+    private String initials;
+    private String avatarColor;
 }
