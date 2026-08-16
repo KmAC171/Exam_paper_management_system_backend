9cef7ea9048da009a68084b19a92777466e6b3d1
KmAC171 <kmcmadushan2002@gmail.com>
2026-08-16 23:02:52 +0530

    created UserManagementDTO

--- src/main/java/com/exam_paper/backend/dto/UserDTO.java
+++ src/main/java/com/exam_paper/backend/dto/UserDTO.java
@@ -1,22 +1,11 @@
 package com.exam_paper.backend.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDTO {
-    private Long userId;
-    private String fullName;
     private String username;
-    private String email;
+    private String password;
+    private String fullname;
     private String role;
-    private String roleLabel;
-    private String department;
-    private boolean isActive;
-    private String lastLogin;
-    private String initials;
-    private String avatarColor;
 }
---
+++ src/main/java/com/exam_paper/backend/dto/UserManagementDTO.java
@@ -0,0 +1,22 @@
+package com.exam_paper.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserManagementDTO {
+    private Long userId;
+    private String fullName;
+    private String username;
+    private String email;
+    private String role;
+    private String roleLabel;
+    private String department;
+    private boolean isActive;
+    private String lastLogin;
+    private String initials;
+    private String avatarColor;
+}
