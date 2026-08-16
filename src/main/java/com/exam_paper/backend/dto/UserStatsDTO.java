a184cf59dd40636e010471499522ed797108a31c
KmAC171 <kmcmadushan2002@gmail.com>
2026-08-16 23:15:43 +0530

    created UserStatsDTO

---
+++ src/main/java/com/exam_paper/backend/dto/UserStatsDTO.java
@@ -0,0 +1,15 @@
+package com.exam_paper.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStatsDTO {
+    private long totalUsers;
+    private long lecturers;
+    private long moderators;
+    private long activeUsers;
+}
