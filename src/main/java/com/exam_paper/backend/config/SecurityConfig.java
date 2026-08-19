5b5d232cf8dc5e8134e64a6c8ff7fe2c31098a34
KmAC171 <kmcmadushan2002@gmail.com>
2026-08-19 10:32:53 +0530

    security gor user management updated

--- src/main/java/com/exam_paper/backend/config/SecurityConfig.java
+++ src/main/java/com/exam_paper/backend/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/workflow/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_GUEST", "ROLE_MODERATOR", "ROLE_USER")
                         .requestMatchers("/api/reports/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_GUEST")
                         .requestMatchers("/api/notifications/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_GUEST", "ROLE_MODERATOR", "ROLE_USER")
+                        .requestMatchers("/api/users/**").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
