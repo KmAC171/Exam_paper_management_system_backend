3a8e0047d2f4425d44089d9c22ece1a28cf88617
KmAC171 <kmcmadushan2002@gmail.com>
2026-09-05 23:02:57 +0530

    updated activity log part

--- src/main/java/com/exam_paper/backend/repository/ActivityLogRepository.java
+++ src/main/java/com/exam_paper/backend/repository/ActivityLogRepository.java
@@ -9,5 +9,6 @@ public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> 
     List<ActivityLog> findTop10ByOrderByCreatedAtDesc();
     List<ActivityLog> findByPacket_PacketIdOrderByCreatedAtAsc(Long packetId);
     List<ActivityLog> findByPacket_Moderator_UserIdOrderByCreatedAtDesc(Long moderatorId);
+    void deleteByPacket_PacketId(Long packetId);
 }
 
